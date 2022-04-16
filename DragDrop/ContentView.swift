28870b25b3b8a186455ce93e1776d01595fc4aec
袁志健 <jswxyzj@qq.com>
2022-04-16 21:27:31 +0800

    Initial Commit

---
+++ DragDrop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DragDrop
+//
+//  Created by 袁志健 on 2022/4/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DragDrop/DragDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragDropApp.swift
+//  DragDrop
+//
+//  Created by 袁志健 on 2022/4/16.
+//
+
+import SwiftUI
+
+@main
+struct DragDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
